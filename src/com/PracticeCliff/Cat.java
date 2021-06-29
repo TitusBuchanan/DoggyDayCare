4c34817884e9db1d54e1a253752c9aefb046f1ec
titus buchanan <titusbuchananjr@gmail.com>
2021-06-29 18:38:32 -0400

    added new classes

---
+++ src/com/PracticeCliff/Cat.java
@@ -0,0 +1,9 @@
+package com.PracticeCliff;
+
+public class Cat extends Animal{
+    public String pattern;
+
+    public Cat(int legs, String size, boolean isFixed, String name, double weight) {
+        super(legs, size, isFixed, name, weight);
+    }
+}
---
+++ src/com/PracticeCliff/Dog.java
@@ -0,0 +1,30 @@
+package com.PracticeCliff;
+
+public class Dog extends Animal {
+    public String breed;
+    public String snoutLength;
+    public String chipId;
+
+
+    public Dog(int legs,
+               String size,
+               boolean isFixed,
+               String name,
+               double weight,
+               String breed,
+               String snoutLength,
+               String chipId
+    ) {
+
+        super(legs, size, isFixed, name, weight);
+        this.breed = breed;
+        this.snoutLength =snoutLength;
+        this.chipId = chipId;
+    }
+
+    @Override
+    public void speak(){
+        System.out.println("A-woof");
+    }
+
+}
