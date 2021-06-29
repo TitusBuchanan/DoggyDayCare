d88908319a7457b569f71ccc76f524aeb50e6e9f
titus buchanan <titusbuchananjr@gmail.com>
2021-06-29 18:52:04 -0400

    completed animal classes

--- src/com/PracticeCliff/Cat.java
+++ src/com/PracticeCliff/Cat.java
@@ -2,8 +2,23 @@ package com.PracticeCliff;
 
 public class Cat extends Animal{
     public String pattern;
+    public boolean wasStray;
+    public int killCount;
 
-    public Cat(int legs, String size, boolean isFixed, String name, double weight) {
+
+    public Cat(int legs, String size, boolean isFixed, String name, double weight, String pattern, boolean wasStray, int killCount) {
         super(legs, size, isFixed, name, weight);
+        this.pattern = pattern;
+        this.wasStray = wasStray;
+        this.killCount = killCount;
+    }
+
+    @Override
+    public void speak() {
+        if(killCount > 5) {
+            System.out.println("Hiss");
+        } else {
+            System.out.println("Mew");
+        }
     }
 }
