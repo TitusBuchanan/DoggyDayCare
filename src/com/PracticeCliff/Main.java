f533c8f846ca5e00115f73bc9d65179b42d59757
titus buchanan <titusbuchananjr@gmail.com>
2021-06-29 19:01:43 -0400

    completed classes

--- src/com/PracticeCliff/Main.java
+++ src/com/PracticeCliff/Main.java
@@ -3,6 +3,12 @@ package com.PracticeCliff;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello");
+	Animal frog = new Animal(4,"small",false,"Raul",2.0);
+	Cat cat = new Cat(4,"Extra Large",true,"henry",12,"Calico]",true,1);
+	Dog dog = new Dog(2,"Medium",true,"Benji",27.7,"Bulldog","short","b34");
+
+	frog.speak();
+	cat.speak();
+	dog.speak();
     }
 }
